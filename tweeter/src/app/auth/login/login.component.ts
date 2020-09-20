d891d5ec5f4c84f4a6885238b01937d42101eb0b
Marinache Eduard-Florin <71124855+eduardedy09@users.noreply.github.com>
2020-09-20 17:37:49 +0300

    Add files via upload

---
+++ tweeter/src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor() { }
+}
